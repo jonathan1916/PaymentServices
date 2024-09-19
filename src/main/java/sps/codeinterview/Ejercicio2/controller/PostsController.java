20ac18e308c4e83971e8779ec905b1bd3a75b434
Jonathan <jonathan191645@gmail.com>
2024-09-18 23:36:03 -0600

    Update PostsController.java

--- src/main/java/sps/codeinterview/Ejercicio2/controller/PostsController.java
+++ src/main/java/sps/codeinterview/Ejercicio2/controller/PostsController.java
@@ -1,11 +1,26 @@
 package sps.codeinterview.reto2.controller;
 
+mport sps.codeinterview.reto1.model.Product;
+import sps.codeinterview.reto1.repository.ProductRepository;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+
+@RestController
+@RequestMapping("/api/v1/cusrumer")
+@Validated
+
 //basepath: /api
 public class PostsController{
 
-//get: /user/{userId}
-//  public List<?> get(?){
-//    return ?;
-//  }
+    @GetMapping("/{id1}")
+    public String getProductById(@PathVariable Integer id1){
+        return String.format("Parametros Recibidos: id1 %s:", id1)
+    }
 	
 }
